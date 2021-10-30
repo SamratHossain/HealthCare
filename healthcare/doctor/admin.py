39f5802ca88f16a1788a6f8aa634aa839b63ccf8
Samrat Hossain <samratcse17019@gmail.com>
2021-10-31 01:53:24 +0600

    add experience model

--- healthcare/doctor/admin.py
+++ healthcare/doctor/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Qualification
+from .models import Qualification, Specialist, Experience
 
 # Register your models here.
 
 admin.site.register(Qualification)
+admin.site.register(Specialist)
+admin.site.register(Experience)
--- healthcare/doctor/models.py
+++ healthcare/doctor/models.py
@@ -20,3 +20,16 @@ class Qualification(models.Model):
 
     def __str__(self):
         return self.DegreeName
+
+class Experience(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    HospitalName = models.CharField(blank=True, null=True, max_length=50)
+    Designation = models.CharField(blank=True, null=True, max_length=50)
+    Department = models.CharField(blank=True, null=True, max_length=50)
+    EmploymentPeriod = models.CharField(blank=True, null=True, max_length=50)
+    CurrentlyWorking = models.BooleanField(default=False)
+    From = models.DateField()
+    To = models.DateField()
+
+    def __str__(self):
+        return self.Designation
