4ac5bc62ae2fe52595b1c60e82a63d3ffe89b396
Samrat Hossain <samratcse17019@gmail.com>
2021-10-30 23:08:39 +0600

    create user account model

--- healthcare/accounts/models.py
+++ healthcare/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .usermanager import AccountUserManager
 
-# Create your models here.
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    email = models.CharField(unique=True,blank=False, null=False, max_length=50)
+    IsPatient = models.BooleanField(default=False)
+    IsDoctor = models.BooleanField(default=False)
+    JoinAt = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = AccountUserManager()
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.email
