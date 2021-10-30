6bc6a768234f51a1454760d299dc7c0b7616dbbc
Samrat Hossain <samratcse17019@gmail.com>
2021-10-31 01:45:30 +0600

    add model specialist

--- healthcare/doctor/models.py
+++ healthcare/doctor/models.py
@@ -4,6 +4,12 @@ from django.contrib.postgres.fields import ArrayField
 
 User = get_user_model()
 
+class Specialist(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Qualification(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     specialist = ArrayField(models.CharField(max_length=50), blank=True, null=True)
