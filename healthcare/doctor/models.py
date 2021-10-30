3da6bf4fdd848263e499aa2a6d21b3da5e90dec1
Samrat Hossain <samratcse17019@gmail.com>
2021-10-31 01:25:08 +0600

    create model qualifications

--- healthcare/doctor/models.py
+++ healthcare/doctor/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+User = get_user_model()
+
+class Qualification(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    specialist = ArrayField(models.CharField(max_length=50), blank=True, null=True)
+    DegreeName = models.CharField(blank=True,null=True,max_length=50)
+    Institute = models.CharField(blank=True,null=True, max_length=50)
+    Country = models.CharField(blank=True,null=True, max_length=50)
+    PassingYear = models.DateField()
+
+    def __str__(self):
+        return self.DegreeName
