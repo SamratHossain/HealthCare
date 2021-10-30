0a9bf0d5b7c84324b4f1a862b49113676f63b2b3
Samrat Hossain <samratcse17019@gmail.com>
2021-10-31 00:31:34 +0600

    add doctor and patient signup view

--- healthcare/accounts/views.py
+++ healthcare/accounts/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import get_user_model
+from accounts.models import Doctor, Patient
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+
+User = get_user_model()
+
+@api_view(['POST'])
+def DoctorSignUp(request):
+    data = request.data
+    user = User.objects.create(
+        email = data['email'],
+        password = make_password(data['password']),
+        IsDoctor = True
+    )
+    doctor = Doctor.objects.create(
+        User = user,
+        Title = data['Title'],
+        FirstName = data['FirstName'],
+        LastName = data['LastName'],
+        Mobile = data['Mobile'],
+        Gender = data['Gender'],
+        DateOfBirth = data['DateOfBirth'],
+        NidOrPassport = data['NidOrPassport'],
+        RegistrationNumberBMDC = data['RegistrationNumberBMDC']
+    )
+    doctor.save()
+    return Response({'success':'Doctor Register Successfully'})
+
+
+@api_view(['POST'])
+def PatientSignUp(request):
+    data = request.data
+    user = User.objects.create(
+        email = data['email'],
+        password = make_password(data['password']),
+        IsPatient = True
+    )
+    patient = Patient.objects.create(
+        User = user,    
+        FirstName = data['FirstName'],
+        LastName = data['LastName'],
+        Mobile = data['Mobile'],
+        Gender = data['Gender'],
+        DateOfBirth = data['DateOfBirth'],
+        District = data['District']
+    )
+    patient.save()
+    return Response({'success':'Patient Register Successfully'})
+
+
